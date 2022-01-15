fa4d3f50737949a5bd95e164927a421bb2fba767
beksultan <turgunbaaev@gmail.com>
2022-01-15 12:32:19 +0600

    Finished sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { Home, TrendingUp } from '@mui/icons-material';
+import { Home, TrendingUp, Category, Group, Paid, Report, Settings, LocalPostOffice, QuestionAnswer, SwapCalls, ArrowBack } 
+from '@mui/icons-material';
 
 const Sidebar = function() {
     return (
-        <aside className="text-white min-h-screen p-4 bg-slate-800 max-w-xs w-1/5">
+        <aside className="relative text-white min-h-screen p-4 bg-slate-800 max-w-xs w-1/5">
             <h3 className="text-3xl font-bold">LOGO</h3>
             <div className="pt-7">
                 <h5 className="font-semibold text-sm opacity-60">Dashboard</h5>
                 <ul>
-                    <li className="mt-2">
+                    <li className="mt-2 mb-1">
                         <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < Home className="opacity-80 mr-2 mb-1" />
+                            < Home className="opacity-80 mr-2" />
                             Home</a>
                     </li>
-                    <li>
+                    <li className="mb-1">
                         <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < TrendingUp className="opacity-80 mr-2 mb-1" />
+                            < TrendingUp className="opacity-80 mr-2" />
                             Analytics</a>
                     </li>
-                    <li>
+                    <li className="mb-1">
                         <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < Home className="opacity-80 mr-2 mb-1" />
+                            < Group className="opacity-80 mr-2" />
                             Users</a>
                     </li>
                 </ul>    
@@ -28,44 +29,65 @@ const Sidebar = function() {
             <div className="pt-7">
                 <h5 className="font-semibold text-sm opacity-60">Quick menu</h5>
                 <ul>
-                    <li className="mt-2">
+                    <li className="mt-2 mb-1">
                         <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < Home className="opacity-80 mr-2 mb-1" />
-                            Home</a>
+                            < Category className="opacity-80 mr-2" />
+                            Products</a>
                     </li>
-                    <li>
+                    <li className="mb-1">
                         <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < TrendingUp className="opacity-80 mr-2 mb-1" />
-                            Analytics</a>
+                            < Paid className="opacity-80 mr-2" />
+                            Transactions</a>
                     </li>
-                    <li>
+                    <li className="mb-1">
                         <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < Home className="opacity-80 mr-2 mb-1" />
-                            Users</a>
+                            < Report className="opacity-80 mr-2" />
+                            Reports</a>
                     </li>
                 </ul>    
             </div>
             <div className="pt-7">
                 <h5 className="font-semibold text-sm opacity-60">Notifications</h5>
                 <ul>
-                    <li className="mt-2">
-                        <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < Home className="opacity-80 mr-2 mb-1" />
-                            Mail</a>
+                    <li className="mt-2 mb-1">
+                        <a className="flex justify-between items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
+                            <span>< SwapCalls className="opacity-80 mr-2" />
+                                Calls
+                            </span>
+                            <span className="py-1 px-3 bg-green-700 rounded-md">+5</span>
+                        </a>
                     </li>
-                    <li>
-                        <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < TrendingUp className="opacity-80 mr-2 mb-1" />
-                            Feedback</a>
+                    <li className="mb-1">
+                        <a className="flex justify-between items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
+                            <span>
+                                < QuestionAnswer className="opacity-80 mr-2" />
+                                Reviews
+                            </span>
+                            <span className="py-1 px-3 bg-green-700 rounded-md">+285</span>    
+                        </a>
                     </li>
-                    <li className="flex items-center justify-between">
+                    <li className="mb-1">
+                        <a className="flex justify-between items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
+                            <span>< LocalPostOffice className="opacity-80 mr-2" />
+                                Messages
+                            </span>
+                            <span className="py-1 px-3 bg-green-700 rounded-md">+28</span>    
+                        </a>
+                    </li>
+                    <li className="mb-1">
                         <a className="flex items-center p-2 hover:bg-slate-700 rounded-md duration-200" href="#">
-                            < Home className="opacity-80 mr-2 mb-1" />
-                            Messages</a>
-                        <span className="py-1 px-3 bg-green-700 rounded-md">+285</span>
+                            < Settings className="opacity-80 mr-2" />
+                            Settings
+                        </a>
                     </li>
                 </ul>    
             </div>    
+
+            <div className="absolute bottom-0 left-0 p-4">
+                <a className="text-xl font-bold flex items-center p-2" href="#">
+                    < ArrowBack className="opacity-80 mr-2" />
+                    Log out</a>
+            </div>
         </aside>
     )
 }
