afffbcb222c445cf332c47776a2b00657ee4d0d4
beksultan <turgunbaaev@gmail.com>
2022-05-18 01:54:37 +0600

    Fixed the routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import Content from './components/content/Content';
+// import Content from './components/content/Content';
 import Sidebar from './components/sidebar/Sidebar';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+import HomePage from "./components/content/home/HomePage";
+import Analytic from "./components/content/analytics/Analytic";
+import Header from "./components/header/Header"
 
 function App() {
   return (
     <div className="flex">
       <BrowserRouter>
         < Sidebar />
-        < Content />
+        <div className="w-full">
+                < Header />
+                <Routes>
+                    <Route path="/" element={<HomePage/>} />
+                    <Route path="/analytics" element={<Analytic/>} />
+                </Routes>
+            </div>
       </BrowserRouter>
     </div>
   );
